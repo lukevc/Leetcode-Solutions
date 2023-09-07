b95148627e1f4db84f06b605cc5b60e1b34c9649
lukevc <lukevincentcannon@hotmail.co.uk>
2023-09-07 15:55:56 +0100

    Submission 1

---
+++ Employees Earning More Than Their Managers/main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    
+    # Create copy of employee
+    df = employee
+
+    # Merges rows
+    df = df.merge(df, left_on='managerId', right_on='id', suffixes=('', '_manager'))
+
+    # Managers don't have managers so this column is redundant
+    df = df.drop(columns=["managerId_manager"])
+
+    # Create a new column that stores a query
+    df["paid_more"] = df["salary"] > df["salary_manager"]
+
+    # Take all the rows that are True
+    filtered_df = df[df['paid_more'] == True]
+
+    # Clean up
+    del df
+
+    # Take the value stored in the 'name' column
+    names = filtered_df['name']
+
+    # Clean up
+    del filtered_df
+
+    # Convert names from a Series to a frame because it is required by leetcode to return Dataframe
+    names = names.to_frame()
+
+    # Rename "names" column to "Employee", because it is also required by leetcode
+    names = names.rename(columns={'name': 'Employee'})
+
+    # return dataframe
+    return names
+    
+find_employees(pd.read_csv("pay.csv"))
